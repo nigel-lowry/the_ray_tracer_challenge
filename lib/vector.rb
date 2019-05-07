1f85a0825c9035dbd8922e0c84c88e45f939ca24
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-07 15:09:05 +0100

    Add magnitude method

--- lib/vector.rb
+++ lib/vector.rb
@@ -4,4 +4,8 @@ class Vector < Tuple
   def initialize x, y, z
     super(x, y, z, 0.0)
   end
+
+  def magnitude
+    Math.sqrt(@x ** 2 + @y ** 2 + @z ** 2)
+  end
 end
--- spec/lib/vector_spec.rb
+++ spec/lib/vector_spec.rb
@@ -6,4 +6,46 @@ RSpec.describe Vector do
       expect(Vector.new(4, -4, 3).to_tuple).to eq(Tuple.new(4, -4, 3, 0))
     end
   end
+
+  describe 'magnitude' do
+    context 'x is 1' do
+      it 'is 1' do
+        v = Vector.new(1, 0, 0)
+
+        expect(v.magnitude).to eq(1)
+      end
+    end
+
+    context 'y is 1' do
+      it 'is 1' do
+        v = Vector.new(0, 1, 0)
+
+        expect(v.magnitude).to eq(1)
+      end
+    end
+
+    context 'z is 1' do
+      it 'is 1' do
+        v = Vector.new(0, 0, 1)
+
+        expect(v.magnitude).to eq(1)
+      end
+    end
+
+    context 'x = 1, y = 2, z = 3' do
+      it 'is sqrt(14)' do
+        v = Vector.new(1, 2, 3)
+
+        expect(v.magnitude).to eq(Math.sqrt(14))
+      end
+    end
+
+    context 'x = -1, y = -2, z = -3' do
+      it 'is sqrt(14)' do
+        v = Vector.new(-1, -2, -3)
+
+        expect(v.magnitude).to eq(Math.sqrt(14))
+      end
+    end
+  end
 end
