14c900f4bc4ef14bb465d3ba8f31a559f7eb9b99
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-07 12:34:17 +0100

    Fix typo

--- spec/lib/vector_spec.rb
+++ spec/lib/vector_spec.rb
@@ -2,7 +2,7 @@ require 'vector'
 
 RSpec.describe Vector do
   describe 'to_tuple' do
-    it 'has w = 1' do
+    it 'has w = 0' do
       expect(Vector.new(4, -4, 3).to_tuple).to eq(Tuple.new(4, -4, 3, 0))
     end
   end
