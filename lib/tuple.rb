4ddce3e57c8a79740ec206b53c2a52aba543cc1c
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-07 12:13:16 +0100

    Extract methods

--- lib/tuple.rb
+++ lib/tuple.rb
@@ -2,11 +2,9 @@ class Tuple
   attr_reader :x, :y, :z
 
   def initialize x, y, z, w
-    raise 'x must be a number' unless x.is_a? Numeric
-    raise 'y must be a number' unless y.is_a? Numeric
-    raise 'z must be a number' unless z.is_a? Numeric
-    
-    raise 'w must be 0.0 or 1.0' unless w == 0.0 or w == 1.0
+    raise_error_if_components_arent_numeric x, y, z
+    raise_error_if_w_isnt_zero_or_one w
+
     @x, @y, @z, @w = x, y, z, w
   end
 
@@ -16,5 +14,17 @@ class Tuple
 
   def vector?
     @w == 0.0
+  end
+
+private
+
+  def raise_error_if_components_arent_numeric x, y, z
+    raise 'x must be a number' unless x.is_a? Numeric
+    raise 'y must be a number' unless y.is_a? Numeric
+    raise 'z must be a number' unless z.is_a? Numeric
+  end
+
+  def raise_error_if_w_isnt_zero_or_one w
+    raise 'w must be 0.0 or 1.0' unless [0.0, 1.0].include? w
   end
 end
