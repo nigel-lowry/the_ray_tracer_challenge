6332909b3b0e7f406d06849a3cff822ec67c4096
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-07 11:56:11 +0100

    Add w check

--- lib/tuple.rb
+++ lib/tuple.rb
@@ -2,6 +2,7 @@ class Tuple
   attr_reader :x, :y, :z
 
   def initialize x, y, z, w
+    raise 'w must be 0.0 or 1.0' unless w == 0.0 or w == 1.0
     @x, @y, @z, @w = x, y, z, w
   end
 
--- spec/lib/tuple_spec.rb
+++ spec/lib/tuple_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe Tuple do
     it { is_expected.to be_a_vector }
     it { is_expected.to_not be_a_point }
   end
+
+  describe 'w' do
+    context 'invalid values' do
+      it 'errors' do
+        invalid_w_values = [-0.1, 0.1, 0.9, 1.1]
+
+        invalid_w_values.each do |value|
+          expect { Tuple.new(4.3, -4.2, 3.1, value) }.to raise_error 'w must be 0.0 or 1.0'
+        end
+      end
+    end
+  end
 end
