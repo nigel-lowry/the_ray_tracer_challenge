e4f8e8e6b7666d3f337c8d2cf121b144486a8180
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-09 20:05:43 +0100

    Refactor to subject

--- spec/lib/factory_spec.rb
+++ spec/lib/factory_spec.rb
@@ -3,27 +3,17 @@ require 'factory'
 RSpec.describe Factory do
   describe '.create' do
     context 'point tuple' do
-      let(:result) { Factory.create(Tuple.new(4.3, -4.2, 3.1, 1.0)) }
+      subject { Factory.create(Tuple.new(4.3, -4.2, 3.1, 1.0)) }
 
-      it 'has x, y and z components' do
-        expect(result).to have_attributes(x: 4.3, y: -4.2, z: 3.1)
-      end
-
-      it 'creates a point' do
-        expect(result).to be_an_instance_of(Point)
-      end
+      it { is_expected.to be_an_instance_of(Point) }
+      it { is_expected.to have_attributes(x: 4.3, y: -4.2, z: 3.1) }
     end
 
     context 'vector tuple' do
-      let(:result) { Factory.create(Tuple.new(4, -4, 3, 0.0)) }
+      subject { Factory.create(Tuple.new(4, -4, 3, 0.0)) }
 
-      it 'has x, y and z components' do
-        expect(result).to have_attributes(x: 4, y: -4, z: 3)
-      end
-
-      it 'creates a vector' do
-        expect(result).to be_an_instance_of(Vector)
-      end
+      it { is_expected.to be_an_instance_of(Vector) }
+      it { is_expected.to have_attributes(x: 4, y: -4, z: 3) }
     end
   end
 end
