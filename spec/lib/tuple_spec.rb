67362a62ba062a9500850cc9e9bfe4eb77ef9761
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-09 20:32:30 +0100

    Refactor to it

--- spec/lib/tuple_spec.rb
+++ spec/lib/tuple_spec.rb
@@ -14,10 +14,7 @@ RSpec.describe Tuple do
   context 'w = 0' do
     subject { Tuple.new(4.3, -4.2, 3.1, 0.0) }
 
-    its(:x) { is_expected.to eq(4.3) }
-    its(:y) { is_expected.to eq(-4.2) }
-    its(:z) { is_expected.to eq(3.1) }
-
+    it { is_expected.to have_attributes(x: 4.3, y: -4.2, z: 3.1, w: 0.0) }
     it { is_expected.to be_a_vector }
     it { is_expected.to_not be_a_point }
   end
