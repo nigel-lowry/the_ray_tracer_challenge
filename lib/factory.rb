341171ee7febcbacfd73ef8ac95196c976d78317
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-09 12:51:57 +0100

    Add vector to factory

--- lib/factory.rb
+++ lib/factory.rb
@@ -3,6 +3,8 @@ class Factory
     case tuple.w
     when 1.0
       Point.new(tuple.x, tuple.y, tuple.z)
+    when 0.0
+      Vector.new(tuple.x, tuple.y, tuple.z)
     else
       raise 'neither a point nor a vector'
     end
--- spec/lib/factory_spec.rb
+++ spec/lib/factory_spec.rb
@@ -5,12 +5,24 @@ RSpec.describe Factory do
     context 'point tuple' do
       let(:result) { Factory.create(Tuple.new(4.3, -4.2, 3.1, 1.0)) }
 
-      it 'has the x, y and z components set' do
+      it 'has x, y and z components' do
         expect(result).to have_attributes(x: 4.3, y: -4.2, z: 3.1)
       end
 
       it 'creates a point' do
         expect(result).to be_an_instance_of(Point)
+      end
+    end
+
+    context 'vector tuple' do
+      let(:result) { Factory.create(Tuple.new(4, -4, 3, 0.0)) }
+
+      it 'has x, y and z components' do
+        expect(result).to have_attributes(x: 4, y: -4, z: 3)
+      end
+
+      it 'creates a vector' do
+        expect(result).to be_an_instance_of(Vector)
       end
     end
   end
