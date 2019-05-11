237f50a8dfaeb8390bd691c82065d2cb3393af5d
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-11 08:17:35 +0100

    Refactor

--- spec/lib/color_spec.rb
+++ spec/lib/color_spec.rb
@@ -1,51 +1,45 @@
 require 'color'
 
 RSpec.describe Color do
-  describe 'construction' do
-    let(:color) { Color.new(-0.5, 0.4, 1.7) }
+  describe '.new' do
+    subject { Color.new(-0.5, 0.4, 1.7) }
 
-    it 'stores the red, green, blue values' do
-      expect(color).to have_attributes(red: -0.5, green: 0.4, blue: 1.7)
-    end
+    it { is_expected.to have_attributes(red: -0.5, green: 0.4, blue: 1.7) }
   end
 
-  describe '+' do
+  describe '#+' do
     let(:c1) { Color.new(0.9, 0.6, 0.75) }
     let(:c2) { Color.new(0.7, 0.1, 0.25) }
+    subject { c1 + c2 }
 
-    it 'adds the components' do
-      expect(c1 + c2).to eq(Color.new(1.6, 0.7, 1.0))
-    end
+    it { is_expected.to eq(Color.new(1.6, 0.7, 1.0)) }
   end
 
-  describe '-' do
+  describe '#-' do
     let(:c1) { Color.new(0.9, 0.6, 0.75) }
     let(:c2) { Color.new(0.7, 0.1, 0.25) }
+    subject { c1 - c2 }
 
-    it 'subtracts the components' do
-      expect(c1 - c2).to closely_eq(Color.new(0.2, 0.5, 0.5))
-    end
+    it { is_expected.to closely_eq(Color.new(0.2, 0.5, 0.5)) }
   end
 
-  describe '*' do
-    context 'color by scalar' do
+  describe '#*' do
+    context 'times scalar' do
       let(:c) { Color.new(0.2, 0.3, 0.4) }
+      subject { c * 2 }
 
-      it 'multiplies the components' do
-        expect(c * 2).to eq(Color.new(0.4, 0.6, 0.8))
-      end
+      it { is_expected.to eq(Color.new(0.4, 0.6, 0.8)) }
     end
 
-    context 'color by color' do
+    context 'times color' do
       let(:c1) { Color.new(1, 0.2, 0.4) }
       let(:c2) { Color.new(0.9, 1, 0.1) }
+      subject { c1 * c2 }
 
-      it 'multiplies each individual component' do
-        expect(c1 * c2).to closely_eq(Color.new(0.9, 0.2, 0.04))
-      end
+      it { is_expected.to closely_eq(Color.new(0.9, 0.2, 0.04)) }
     end
 
-    context 'color by string' do
+    context 'times vector' do
       let(:c) { Color.new(1, 0.2, 0.4) }
       let(:v) { Vector.new(1, 2, 3) }
 
