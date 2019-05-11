6dc7d5fc3026b2dfefe36a89a80540bf4ad906e1
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-11 10:18:25 +0100

    Add access methods

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -3,10 +3,18 @@ class Canvas
 
   def initialize width, height
     @width, @height = width, height
-    @array = Array.new(width) { Array.new(height) { Color.new(0, 0, 0) } }
+    @array = Array.new(height) { Array.new(width) { Color.new(0, 0, 0) } }
   end
 
   def to_a
     @array.flatten
+  end
+
+  def write_pixel(x, y, color)
+    @array[y][x] = color
+  end
+
+  def pixel_at(x, y)
+    @array[y][x]
   end
 end
--- spec/lib/canvas_spec.rb
+++ spec/lib/canvas_spec.rb
@@ -1,13 +1,22 @@
 require 'canvas'
 
 RSpec.describe Canvas do
-  describe '.new' do
-    subject { Canvas.new(10, 20) }
+  subject { Canvas.new(10, 20) }
 
+  describe '.new' do
     it { is_expected.to have_attributes(width: 10, height: 20) }
 
     specify 'every pixel is black' do
       expect(subject.to_a).to all be_black
+    end
+  end
+
+  describe '#write_pixel' do
+    let(:red) { Color.new(1, 0, 0) }
+    before { subject.write_pixel(2, 3, red) }
+
+    it 'changes the pixel color' do
+      expect(subject.pixel_at(2, 3)).to eq(red)
     end
   end
 end
