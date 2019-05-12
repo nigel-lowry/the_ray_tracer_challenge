066f52c60aab5661b66c869f21f600e5c0fa0555
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-12 20:01:40 +0100

    Add tuple multiplication

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -10,6 +10,28 @@ class Matrix
   end
 
   def *(other)
+    if other.is_a? Matrix
+      multiply_by_matrix other
+    elsif other.is_a? Tuple
+      a = [0, 0, 0, 0]
+
+      for row in 0..3
+        a[row] =  get(row, 0) * other.x +
+                  get(row, 1) * other.y +
+                  get(row, 2) * other.z +
+                  get(row, 3) * other.w
+      end
+
+      Tuple.new(a[0], a[1], a[2], a[3])
+    end
+  end
+
+  def ==(other)
+    @data.flatten == other.data.flatten
+  end
+
+private
+  def multiply_by_matrix matrix
     a = [[0, 0, 0, 0],
          [0, 0, 0, 0],
          [0, 0, 0, 0],
@@ -17,17 +39,13 @@ class Matrix
 
     for row in 0..3
       for col in 0..3
-        a[row][col] = get(row, 0) * other.get(0, col) +
-                      get(row, 1) * other.get(1, col) +
-                      get(row, 2) * other.get(2, col) +
-                      get(row, 3) * other.get(3, col)
+        a[row][col] = get(row, 0) * matrix.get(0, col) +
+                      get(row, 1) * matrix.get(1, col) +
+                      get(row, 2) * matrix.get(2, col) +
+                      get(row, 3) * matrix.get(3, col)
       end
     end
 
     Matrix.new a
-  end
-
-  def ==(other)
-    @data.flatten == other.data.flatten
   end
 end
