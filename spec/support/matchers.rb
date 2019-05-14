0dd4a70f0991723ba41a81dbc5c8eeca565c7858
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-14 15:07:49 +0100

    Refactor

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -2,9 +2,9 @@ EPSILON = 0.00001
 
 RSpec::Matchers.define :closely_eq do |expected|
   match do |actual|
-    if (expected.class == Color and actual.class == Color) || (expected.class == Point and actual.class == Point) || (expected.class == Vector and actual.class == Vector)
-      numbers_close?(actual.x, expected.x) && numbers_close?(actual.y, expected.y) && numbers_close?(actual.z, expected.z) && numbers_close?(actual.w, expected.w)
-    elsif expected.class == Matrix and actual.class == Matrix
+    if composed_of_a_tuple?(expected) and composed_of_a_tuple?(actual) and same_class?(expected, actual)
+      numbers_close?(actual.x, expected.x) and numbers_close?(actual.y, expected.y) and numbers_close?(actual.z, expected.z) && numbers_close?(actual.w, expected.w)
+    elsif both_matrices?(expected, actual)
       return false unless expected.size == actual.size
 
       for row in 0...expected.data.length
@@ -21,7 +21,19 @@ RSpec::Matchers.define :closely_eq do |expected|
 
 private 
 
+  def composed_of_a_tuple? a
+    a.respond_to? :tuple
+  end
+
+  def same_class? a, b
+    a.class == b.class
+  end
+
   def numbers_close? a, b
     (a - b).abs < EPSILON
+  end
+
+  def both_matrices? a, b
+    a.class == Matrix and b.class == Matrix
   end
 end
