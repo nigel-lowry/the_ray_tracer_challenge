4f185435b4243422f7ed4694eed83531d94bfbce
Nigel Lowry <nigel@lemmata.co.uk>
2019-05-14 15:01:56 +0100

    Apply to other classes

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -2,7 +2,7 @@ EPSILON = 0.00001
 
 RSpec::Matchers.define :closely_eq do |expected|
   match do |actual|
-    if expected.class == Color and actual.class == Color
+    if (expected.class == Color and actual.class == Color) || (expected.class == Point and actual.class == Point) || (expected.class == Vector and actual.class == Vector)
       numbers_close?(actual.x, expected.x) && numbers_close?(actual.y, expected.y) && numbers_close?(actual.z, expected.z) && numbers_close?(actual.w, expected.w)
     elsif expected.class == Matrix and actual.class == Matrix
       return false unless expected.size == actual.size
