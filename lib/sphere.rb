5cdaa71ce8432aabdbf5e9789955ae121f76288f
Nigel Lowry <nigel@lemmata.co.uk>
2019-06-07 17:14:28 +0100

    Specs passing but need to remove duplicated code

--- lib/sphere.rb
+++ lib/sphere.rb
@@ -1,7 +1,34 @@
 class Sphere
   attr_reader :radius
-  
+
   def initialize
     @radius = 1
+  end
+
+  def intersect ray
+    if discriminant(ray) < 0
+      []
+    else
+      sphere_to_ray = ray.origin - Point.new(0, 0, 0) # const
+      a = Vector.dot(ray.direction, ray.direction)
+      b = 2 * Vector.dot(ray.direction, sphere_to_ray)
+
+      t1 = (-b - Math.sqrt(discriminant(ray))) / (2 * a)
+      t2 = (-b + Math.sqrt(discriminant(ray))) / (2 * a)
+
+      [t1, t2]
+    end
+  end
+
+private
+
+  def discriminant ray
+    sphere_to_ray = ray.origin - Point.new(0, 0, 0) # const
+
+    a = Vector.dot(ray.direction, ray.direction)
+    b = 2 * Vector.dot(ray.direction, sphere_to_ray)
+    c = Vector.dot(sphere_to_ray, sphere_to_ray) - 1
+
+    b * b - 4 * a * c
   end
 end
--- spec/lib/sphere_spec.rb
+++ spec/lib/sphere_spec.rb
@@ -10,4 +10,69 @@ RSpec.describe Sphere do
       expect(subject).not_to be(Sphere.new)
     end
   end
+
+  describe '#intersect' do
+    context 'not tangential' do
+      let(:r) { Ray.new(Point.new(0, 0, -5), Vector.new(0, 0, 1)) }
+      let(:s) { Sphere.new }
+
+      it 'intersects at two points' do
+        xs = s.intersect(r)
+
+        expect(xs.count).to eq(2)
+        expect(xs.first).to eq(4.0)
+        expect(xs.last).to eq(6.0)
+      end
+    end
+
+    context 'at a tangent' do
+      let(:r) { Ray.new(Point.new(0, 1, -5), Vector.new(0, 0, 1)) }
+      let(:s) { Sphere.new }
+
+      it 'intersects at one point' do
+        xs = s.intersect(r)
+
+        expect(xs.count).to eq(2)
+        expect(xs.first).to eq(5.0)
+        expect(xs.last).to eq(5.0)
+      end
+    end
+
+    context 'not intersecting' do
+      let(:r) { Ray.new(Point.new(0, 2, -5), Vector.new(0, 0, 1)) }
+      let(:s) { Sphere.new }
+
+      it 'has no intersections' do
+        xs = s.intersect(r)
+
+        expect(xs).to be_empty
+      end
+    end
+
+    context 'ray originate inside sphere' do
+      let(:r) { Ray.new(Point.new(0, 0, 0), Vector.new(0, 0, 1)) }
+      let(:s) { Sphere.new }
+
+      it 'has an intersection behind the starting point too' do
+        xs = s.intersect(r)
+
+        expect(xs.count).to eq(2)
+        expect(xs.first).to eq(-1.0)
+        expect(xs.last).to eq(1.0)
+      end
+    end
+
+    context 'ray originates after sphere' do
+      let(:r) { Ray.new(Point.new(0, 0, 5), Vector.new(0, 0, 1)) }
+      let(:s) { Sphere.new }
+
+      it 'has two intersections behind' do
+        xs = s.intersect(r)
+
+        expect(xs.count).to eq(2)
+        expect(xs.first).to eq(-6.0)
+        expect(xs.last).to eq(-4.0)
+      end
+    end
+  end
 end
