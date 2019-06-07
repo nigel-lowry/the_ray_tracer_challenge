86d712679eaeaab105e73d65fe8250df0067126f
Nigel Lowry <nigel@lemmata.co.uk>
2019-06-07 17:23:22 +0100

    Refactor to class but still calling twice

--- lib/sphere.rb
+++ lib/sphere.rb
@@ -9,9 +9,8 @@ class Sphere
     if discriminant(ray) < 0
       []
     else
-      sphere_to_ray = ray.origin - Point.new(0, 0, 0) # const
-      a = Vector.dot(ray.direction, ray.direction)
-      b = 2 * Vector.dot(ray.direction, sphere_to_ray)
+      dc = DiscriminantCalculator.new(self, ray)
+      a, b = dc.a, dc.b
 
       t1 = (-b - Math.sqrt(discriminant(ray))) / (2 * a)
       t2 = (-b + Math.sqrt(discriminant(ray))) / (2 * a)
@@ -23,12 +22,21 @@ class Sphere
 private
 
   def discriminant ray
-    sphere_to_ray = ray.origin - Point.new(0, 0, 0) # const
-
-    a = Vector.dot(ray.direction, ray.direction)
-    b = 2 * Vector.dot(ray.direction, sphere_to_ray)
-    c = Vector.dot(sphere_to_ray, sphere_to_ray) - 1
+    dc = DiscriminantCalculator.new(self, ray)
+    sphere_to_ray, a, b, c = dc.sphere_to_ray, dc.a, dc.b, dc.c
 
     b * b - 4 * a * c
+  end
+end
+
+class DiscriminantCalculator
+  attr_reader :sphere_to_ray, :a, :b, :c
+
+  def initialize sphere, ray
+    @sphere_to_ray = ray.origin - Point.new(0, 0, 0) # const
+
+    @a = Vector.dot(ray.direction, ray.direction)
+    @b = 2 * Vector.dot(ray.direction, sphere_to_ray)
+    @c = Vector.dot(sphere_to_ray, sphere_to_ray) - 1
   end
 end
