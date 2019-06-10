9f6c9915ed492039f0f419744018de29fcccc624
Nigel Lowry <nigel@lemmata.co.uk>
2019-06-10 21:34:18 +0100

    Add specs

--- lib/point_light.rb
+++ lib/point_light.rb
@@ -1,2 +1,10 @@
+require 'point'
+require 'color'
+
 class PointLight
+  attr_reader :position, :intensity
+
+  def initialize position, intensity
+    @position, @intensity = position, intensity
+  end
 end
--- spec/lib/point_light_spec.rb
+++ spec/lib/point_light_spec.rb
@@ -1,4 +1,12 @@
 require 'point_light'
 
 RSpec.describe PointLight do
+  describe '.new' do
+    let(:position) { Point.new(0, 0, 0) }
+    let(:intensity) { Color.new(1, 1, 1) }
+    let(:light) { PointLight.new(position, intensity)}
+
+    specify { expect(light.position).to eq(position) }
+    specify { expect(light.intensity).to eq(intensity) }
+  end
 end
