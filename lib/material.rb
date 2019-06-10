b253316ff7dcee935354c38bd3de96688812b284
Nigel Lowry <nigel@lemmata.co.uk>
2019-06-10 22:27:15 +0100

    Specs passing

--- lib/material.rb
+++ lib/material.rb
@@ -1,4 +1,5 @@
 require 'color'
+require 'point_light'
 
 class Material
   attr_accessor :color, :ambient, :diffuse, :specular, :shininess
@@ -9,5 +10,32 @@ class Material
 
   def ==(other)
     self.class == other.class and self.color == other.color and self.ambient == other.ambient and self.diffuse == other.diffuse and self.specular == other.specular and self.shininess == other.shininess
+  end
+
+  def lighting light, point, eye_v, normal_v
+    effective_color = color * light.intensity
+    light_v = (light.position - point).normalize
+    ambient_contribution = effective_color * ambient
+
+    light_dot_normal = Vector.dot(light_v, normal_v)
+
+    if light_dot_normal < 0
+      diffuse_contribution = Color::BLACK
+      specular_contribution = Color::BLACK
+    else
+      diffuse_contribution = effective_color * diffuse * light_dot_normal
+
+      reflect_v = -light_v.reflect(normal_v)
+      reflect_dot_eye = Vector.dot(reflect_v, eye_v)
+
+      if reflect_dot_eye <= 0
+        specular_contribution = Color::BLACK
+      else
+        factor = reflect_dot_eye ** shininess
+        specular_contribution = light.intensity * specular * factor
+      end
+    end
+
+    ambient_contribution + diffuse_contribution + specular_contribution
   end
 end
--- spec/lib/material_spec.rb
+++ spec/lib/material_spec.rb
@@ -10,4 +10,51 @@ RSpec.describe Material do
     its(:specular) { is_expected.to eq(0.9) }
     its(:shininess) { is_expected.to eq(200.0) }
   end
+
+  describe '#lighting' do
+    let(:m) { Material.new }
+    let(:position) { Point::ORIGIN }
+
+    context 'lighting with the eye between the light and the surface' do
+      let(:eye_v) { Vector.new(0, 0, -1) }
+      let(:normal_v) { Vector.new(0, 0, -1) }
+      let(:light) { PointLight.new(Point.new(0, 0, -10), Color.new(1, 1, 1)) }
+
+      specify { expect(m.lighting(light, position, eye_v, normal_v)).to eq(Color.new(1.9, 1.9, 1.9)) }
+    end
+
+    context 'lighting with the eye between the light and the surface, eye offset 45 degrees' do
+      let(:sqrt_of_2_divided_by_2) { Math::sqrt(2) / 2 }
+      let(:eye_v) { Vector.new(0, sqrt_of_2_divided_by_2, -sqrt_of_2_divided_by_2) }
+      let(:normal_v) { Vector.new(0, 0, -1) }
+      let(:light) { PointLight.new(Point.new(0, 0, -10), Color.new(1, 1, 1)) }
+
+      specify { expect(m.lighting(light, position, eye_v, normal_v)).to eq(Color.new(1.0, 1.0, 1.0)) }
+    end
+
+    context 'lighting with the eye opposite surface, light offset 45 degrees' do
+      let(:eye_v) { Vector.new(0, 0, -1) }
+      let(:normal_v) { Vector.new(0, 0, -1) }
+      let(:light) { PointLight.new(Point.new(0, 10, -10), Color.new(1, 1, 1)) }
+
+      specify { expect(m.lighting(light, position, eye_v, normal_v)).to closely_eq(Color.new(0.7364, 0.7364, 0.7364)) }
+    end
+
+    context 'lighting with the eye in the path of the reflection vector' do
+      let(:sqrt_of_2_divided_by_2) { Math::sqrt(2) / 2 }
+      let(:eye_v) { Vector.new(0, -sqrt_of_2_divided_by_2, -sqrt_of_2_divided_by_2) }
+      let(:normal_v) { Vector.new(0, 0, -1) }
+      let(:light) { PointLight.new(Point.new(0, 10, -10), Color.new(1, 1, 1)) }
+
+      specify { expect(m.lighting(light, position, eye_v, normal_v)).to closely_eq(Color.new(1.6364, 1.6364, 1.6364)) }
+    end
+
+    context 'lighting with the light behind the surface' do
+      let(:eye_v) { Vector.new(0, 0, -1) }
+      let(:normal_v) { Vector.new(0, 0, -1) }
+      let(:light) { PointLight.new(Point.new(0, 0, 10), Color.new(1, 1, 1)) }
+
+      specify { expect(m.lighting(light, position, eye_v, normal_v)).to closely_eq(Color.new(0.1, 0.1, 0.1)) }
+    end
+  end
 end
