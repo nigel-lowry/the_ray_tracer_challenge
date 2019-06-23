ea91ef50a8a27575c1bb815c111f3faa66bf97d6
Nigel Lowry <nigel@lemmata.co.uk>
2019-06-23 22:30:10 +0100

    Add specs

--- lib/camera.rb
+++ lib/camera.rb
@@ -1,3 +1,10 @@
+require 'transform'
+
 class Camera
-  
+  attr_reader :hsize, :vsize, :field_of_view, :transform
+
+  def initialize hsize, vsize, field_of_view
+    @hsize, @vsize, @field_of_view = hsize, vsize, field_of_view
+    @transform = Transform::IDENTITY
+  end
 end
--- spec/lib/camera_spec.rb
+++ spec/lib/camera_spec.rb
@@ -1,5 +1,16 @@
 require 'camera'
 
 RSpec.describe Camera do
+  describe '.new' do
+    let(:hsize) { 160 }
+    let(:vsize) { 120 }
+    let(:field_of_view) { Math::PI / 2 }
 
+    subject { Camera.new(hsize, vsize, field_of_view) }
+
+    its(:hsize) { is_expected.to eq(160) }
+    its(:vsize) { is_expected.to eq(120) }
+    its(:field_of_view) { is_expected.to eq(Math::PI / 2) }
+    its(:transform) { is_expected.to eq(Transform::IDENTITY) }
+  end
 end
