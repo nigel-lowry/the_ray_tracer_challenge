d4d18d032900db7f7359dbb56c2874f1938a0ace
Nigel Lowry <nigel@lemmata.co.uk>
2019-06-23 18:53:28 +0100

    Add inside method

--- lib/prepare_computations.rb
+++ lib/prepare_computations.rb
@@ -1,7 +1,7 @@
 require 'intersection'
 
 class PrepareComputations
-  attr_reader :t, :object, :point, :eyev, :normalv
+  attr_reader :t, :object, :point, :eyev, :normalv, :inside
 
   def initialize intersection, ray
     @t = intersection.t
@@ -10,5 +10,12 @@ class PrepareComputations
     @point = ray.position(@t)
     @eyev = -ray.direction
     @normalv = @object.normal_at @point
+
+    @inside = if Vector.dot(@normalv, @eyev) < 0
+      @normalv = -@normalv
+      true
+    else
+      false
+    end
   end
 end
--- spec/lib/prepare_computations_spec.rb
+++ spec/lib/prepare_computations_spec.rb
@@ -12,5 +12,26 @@ RSpec.describe PrepareComputations do
     specify { expect(comps.point).to eq(Point.new(0, 0, -1)) }
     specify { expect(comps.eyev).to eq(Vector.new(0, 0, -1)) }
     specify { expect(comps.normalv).to eq(Vector.new(0, 0, -1)) }
+
+    context 'intersection on outside' do
+      let(:r) { Ray.new(Point.new(0, 0, -5), Vector.new(0, 0, 1)) }
+      let(:shape) { Sphere.new }
+      let(:i) { Intersection.new(4, shape) }
+      let(:comps) { PrepareComputations.new(i, r) }
+
+      specify { expect(comps.inside).to be false }
+    end
+
+    context 'intersection on inside' do
+      let(:r) { Ray.new(Point.new(0, 0, 0), Vector.new(0, 0, 1)) }
+      let(:shape) { Sphere.new }
+      let(:i) { Intersection.new(1, shape) }
+      let(:comps) { PrepareComputations.new(i, r) }
+
+      specify { expect(comps.point).to eq(Point.new(0, 0, 1)) }
+      specify { expect(comps.eyev).to eq(Vector.new(0, 0, -1)) }
+      specify { expect(comps.inside).to be true }
+      specify { expect(comps.normalv).to eq(Vector.new(0, 0, -1)) }
+    end
   end
 end
