767e2ef371f541d078a792a88127da1ec703863a
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2021-08-11 22:40:32 +0100

    Add GradientPattern

---
+++ spec/lib/gradient_pattern_spec.rb
@@ -0,0 +1,13 @@
+require 'gradient_pattern'
+
+RSpec.describe GradientPattern do
+  let(:color_a) { Color::WHITE }
+  let(:color_b) { Color::BLACK }
+
+  subject { GradientPattern.new(a: color_a, b: color_b) }
+
+  specify { expect(subject.pattern_at(Point.new(0, 0, 0))).to eq(color_a) }
+  specify { expect(subject.pattern_at(Point.new(0.25, 0, 0))).to eq(Color.new(0.75, 0.75, 0.75)) }
+  specify { expect(subject.pattern_at(Point.new(0.5, 0, 0))).to eq(Color.new(0.5, 0.5, 0.5)) }
+  specify { expect(subject.pattern_at(Point.new(0.75, 0, 0))).to eq(Color.new(0.25, 0.25, 0.25)) }
+end
